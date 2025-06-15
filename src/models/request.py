32ca0d882b91571c2ed77ff91605810b3d6d907b
v_amerk <devamerkh@gmail.com>
2025-06-15 16:23:56 +0500

    поле retooler в запросе теперь необязательно

--- src/models/request.py
+++ src/models/request.py
@@ -70,7 +70,7 @@ class RetoolerSetting(BaseModel):
 
 class ProductionSpecification(BaseModel):
     directory: Directory
-    retooler: RetoolerSetting
+    retooler: RetoolerSetting = Field(default_factory=lambda: RetoolerSetting(height=0, width=0))
     ready_plates: List[PlateSpecification] = Field(default_factory=list)
     available_tracks: List[AvailableTrack] = Field(default_factory=list)
     orders: List[Order] = Field(default_factory=list)
